015bec663cc7406b9e384cdf852fd66b368e0a5f
unknown <daniel2001.cieslak@wp.pl>
2025-03-29 18:53:32 +0100

    Dodanie ikony aplikacji

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
-from app.MainWindow import MainWindow
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+
+from app.MainWindow import MainWindow
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("app\\icons\\icon.ico"))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
